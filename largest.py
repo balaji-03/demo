98dff61637dc51134939a20f15679c6817a6d255
balaji-03 <balaji.s@acufore.com>
2022-01-04 17:01:05 +0530

    Updated on 22FEB

---
+++ largest.py
@@ -0,0 +1,11 @@
+num=[]
+number=int(input("Enter the number of elements in  a list"))
+for i in range(1,number+1):
+    value=int(input("Enter the %d element in array"%i))
+    num.append(value)
+temp=num[0]
+for j in range(1,number):
+    if num[j]>temp: 
+       large=num[j]
+       temp=num[j]
+print(large)
