6d7d8301e543afd82008e9405f7aacba05220563
balaji-03 <balaji.s@acufore.com>
2022-01-13 12:04:06 +0530

    uploaded all

--- tail.py
+++ tail.py
@@ -1,18 +1,17 @@
 a=open("D:\\Balaji\\Git\\Demo\\file2.txt","r")
 def no_lines():
-    c=1
+    c=0
     for line in a:
         c=c+1
     return c
 d=no_lines()
-e=input("enter the lines")
-f=input("enter file name")
-s=input("enter the function name :")
-def lastline(a,b):
-    b=open("D:\\Balaji\\Git\\Demo\\"+b+'.'+'txt')   
-    user=int(a)
+e,l,z,k,m= [str(e) for e in input("Enter function name: ").split()]
+s=e+l+z+k+m
+def lastline(f,c):
+    b=open("D:\\Balaji\\Git\\Demo\\"+c+'.'+'txt')   
+    user=int(f)
     for i,line1 in enumerate(b):
-        if i>=d-(user+1):
-            print(line1)    
-if s == 'tail('+e+','+f+')':                
-    lastline(e,f)
+        if i>=d-(user):
+            print(line1)
+if s == 'tail('+l+z+k+')':   
+    lastline(l,k)
